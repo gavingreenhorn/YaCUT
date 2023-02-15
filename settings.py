bdc1abd28e00dd5ec83789b8c0cd7e21ef078643
Gavin Greenhorn <gavingreenhorn@gmail.com>
2023-02-15 21:36:57 +0300

    1st take

--- settings.py
+++ settings.py
@@ -2,6 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    # не проходит не-локальный pytest
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    # SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+    SECRET_KEY = 'whatever'
